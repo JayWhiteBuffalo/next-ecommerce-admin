74a88de1cac53c2e9e1467692a059b0907b0b614
White Buffalo <apothocare@gmail.com>
2023-04-20 14:47:58 -0500

    Product Edit function works

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,51 +1,12 @@
+
 import Layout from "@/components/Layout";
-import { useState } from "react";
-import axios from "axios";
-import { useRouter } from "next/router";
+import ProductForm from "@/components/ProductForm";
 
 export default function NewProduct(){
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [price, setPrice] = useState('');
-    const [goToProducts, setGoToProducts] = useState(false);
-    const router = useRouter();
-    async function createProduct(e){
-        e.preventDefault();
-        const data = {title,description,price};
-        await axios.post('/api/products', data);
-        setGoToProducts(true);
-    }
-    if (goToProducts) {
-        router.push('/products');
-    }
-    return(
-        <Layout>
-            <form onSubmit={createProduct}>
-                <h1>New Product</h1>
-                <input 
-                    type="text" 
-                    placeholder="product name"
-                    value={title} onChange={e => setTitle(e.target.value)}
-                />
-                <label>Description</label>
-                <textarea 
-                    placeholder="description"
-                    value={description}
-                    onChange={e=> setDescription(e.target.value)}
-                />
-                <label>Price (in USD)</label>        
-                <input 
-                    type="text" 
-                    placeholder="price"
-                    value={price}
-                    onChange={e=> setPrice(e.target.value)}
-                />
-                <button 
-                    className="btn-primary"
-                    type="submit">
-                    Save
-                </button>
-            </form>
-        </Layout>
-    );
+   return (
+    <Layout>
+        <h1>New Product</h1>
+        <ProductForm />
+    </Layout> 
+   )
 }
