6989d8e13987992abf9127625b4c29272d0f1c1a
White Buffalo <apothocare@gmail.com>
2023-04-20 14:05:57 -0500

    Products fetched and rendered on products page in a table.

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,10 +1,16 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
+import axios from "axios";
 
 
 export default async function handle(req, res) {
     const {method} = req;
     await mongooseConnect();
+
+    if (method === 'GET') {
+        res.json(await Product.find());
+    }
+
     if (method === 'POST') {
         const {title,description,price} = req.body;
         const productDoc = await Product.create({
