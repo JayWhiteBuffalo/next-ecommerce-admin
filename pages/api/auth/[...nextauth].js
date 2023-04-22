716df88f9b20e53268ab6edb126b0f7692f7429c
White Buffalo <apothocare@gmail.com>
2023-04-22 14:25:37 -0500

    Added Admin auth for logging into panel

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,11 @@
 import clientPromise from '@/lib/mongodb'
-import NextAuth from 'next-auth'
+import NextAuth, { getServerSession } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 
-export default NextAuth({
+const adminEmails = ['apothocare@gmail.com'];
+
+export const authOptions = {
   providers: [
     // OAuth authentication providers...
     GoogleProvider({
@@ -12,4 +14,22 @@ export default NextAuth({
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-})
+  callbacks: {
+    session: ({session,token,user}) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false
+      }
+      }
+  }
+};
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req,res) {
+  const session = await getServerSession(req,res,authOptions);
+  if (!adminEmails.includes(session?.user?.email)) {
+    throw 'User Account Does not have Admin Access'
+  }
+}
