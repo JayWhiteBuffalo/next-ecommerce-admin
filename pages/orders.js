fec8c4104fbc3bd8b74084dfe38d13abaad48811
White Buffalo <apothocare@gmail.com>
2023-05-22 09:54:31 -0500

    Updated Orders tab on admin panel to show Orders from DB

--- pages/orders.js
+++ pages/orders.js
@@ -1,12 +1,54 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-export default function Orders(){
+export default function OrdersPage(){
+    const [orders, setOrders] = useState([]);
+    useEffect(()=> {
+        axios.get('/api/orders').then(res => {
+            setOrders(res.data);
+        }) ;
+    }, []);
     return(
         <Layout>
-            <Link className="bg-blue-900 rounded-md text-white py-1 px-2" href={'/orders/active'}>
-                Active Orders
-            </Link>
+            <h1>Orders</h1>
+            <table className="basic">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Recipient</th>
+                        <th>Products</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orders.length > 0 && orders.map(order => (
+                        <tr>
+                            <td>
+                                {order.createdAt}
+                            </td>
+                            <td>
+                                {order._id}
+                            </td>
+                            <td>
+                                {order.name} {order.email}
+                                <br/>
+                                {order.city} {order.postalCode} {order.country}
+                                <br/>
+                                {order.streetAddress}
+                            </td>
+                            <td>
+                                {order.line_items.map(item => (
+                                    <>
+                                    {item.price_data?.product_data.name} X {item.quantity} <br/>
+                                    {/* {JSON.stringify(item)}<br/> */}
+                                    </>
+                                ))}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </Layout>
     )
 }
